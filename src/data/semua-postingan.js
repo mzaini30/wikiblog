79988818837730a102995be95c075caac9d9a1a9
mzaini30 <muhzaini30@gmail.com>
2023-06-04 08:02:03 +0800

    simpan dulu

--- src/data/semua-postingan.js
+++ src/data/semua-postingan.js
@@ -1,5 +1,14 @@
 import semuanya from '../tiddlers/published.json'
+import konfigurasi from "../konfigurasi.yml"
 
 export default function () {
-  return semuanya
+  return semuanya.map(x => ({
+    title: x.title,
+    published_date: +x.published_date,
+    slug: x.slug,
+    text: x.text,
+    ringkasan: x.ringkasan || konfigurasi.default.ringkasan,
+    kategori: x.kategori || konfigurasi.default.kategori,
+    gambar: x.gambar || konfigurasi.default.gambar
+  }))
 }
